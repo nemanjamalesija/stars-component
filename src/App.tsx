a63ce0f9d1744985c8616a8ed7964ac0c730b0fe
nemanjamalesija <malesija13@gmail.com>
2023-07-16 14:55:12 +0200

    Add styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import StarRating from './StarRating';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className='container'>
+        <StarRating maxRating={10} />
+      </div>
+    </>
+  );
 }
 
 export default App;
