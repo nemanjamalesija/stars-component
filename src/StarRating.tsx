ca60ca29292a7572b8e4a94818d92067da474269
nemanjamalesija <malesija13@gmail.com>
2023-07-16 14:48:32 +0200

    Add stars

--- src/StarRating.tsx
+++ src/StarRating.tsx
@@ -1,4 +1,3 @@
-import PropTypes from 'prop-types';
 import { useState } from 'react';
 
 const containerStyle = {
@@ -11,13 +10,14 @@ const starContainerStyle = {
   display: 'flex',
 };
 
-interface starRatingPropsType {
+interface StarRatingPropsType {
   maxRating: number;
   defaultRating: number;
   color: string;
   size: number;
   messages: string[];
   className: string;
+  onSetRating: (rating: number) => void;
 }
 
 export default function StarRating({
@@ -27,12 +27,14 @@ export default function StarRating({
   className = '',
   messages = [],
   defaultRating = 0,
-}) {
+  onSetRating,
+}: StarRatingPropsType) {
   const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
 
   function handleRating(rating: number) {
     setRating(rating);
+    onSetRating(rating);
   }
 
   const textStyle = {
@@ -63,5 +65,68 @@ export default function StarRating({
           : tempRating || rating || ''}
       </p>
     </div>
+  );
+}
+
+/* STARS */
+
+interface StarPropsType {
+  key: number;
+  full: boolean;
+  onRate: () => void;
+  onHoverIn: () => void;
+  onHoverOut: () => void;
+  color: string;
+  size: number;
+}
+
+function Star({
+  onRate,
+  full,
+  onHoverIn,
+  onHoverOut,
+  color,
+  size,
+}: StarPropsType) {
+  const starStyle = {
+    width: `${size}px`,
+    height: `${size}px`,
+    display: 'block',
+    cursor: 'pointer',
+  };
+
+  return (
+    <span
+      role='button'
+      style={starStyle}
+      onClick={onRate}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+    >
+      {full ? (
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          viewBox='0 0 20 20'
+          fill={color}
+          stroke={color}
+        >
+          <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
+        </svg>
+      ) : (
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          stroke={color}
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            strokeWidth='{2}'
+            d='M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z'
+          />
+        </svg>
+      )}
+    </span>
   );
 }
